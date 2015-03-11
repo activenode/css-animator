92a2a89fc9be2c0d897212d0116ae47d4f7bc00c
activenode <activenode@users.noreply.github.com>
2015-03-11 09:57:44 +0100

    Update puppy.js

--- puppy.js
+++ puppy.js
@@ -1,41 +1,60 @@
 var cssAnimator = {
-    general: function($elem, addClass, removeClass, finishCallback, isTransition, delayMs) {
-    	var exec = function() {
-    		var transEvents     = 'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend';
-	        var animEvents      = 'animationend oAnimationEnd webkitAnimationEnd';
-	        
-	        var evts = animEvents;
-	        if (isTransition) {
-	            evts = transEvents;
-	        }
+    process: function($elem, modifierFunc, finishedFunc, opts) {
+        var listenTo = opts.listenAtElem ? opts.listenAtElem : $elem;
 
-	        if (removeClass && !$elem.hasClass(removeClass)) {
-	        	finishCallback($elem);
-	        	return;
-	        }
-	        
-	        $elem.one(evts, function(){
-	            $elem.off(evts);
-	            finishCallback($elem);
-	        });
-	        
-	        if (addClass!==false && addClass!==null) {
-	            $elem.addClass(addClass);
-	        }
-	        
-	        if (removeClass!==false && removeClass!==null) {
-	            $elem.removeClass(removeClass);
-	        }
-    	};
+        var eventNs         = '.r'+Math.floor(Math.random()*100000)+' ';
+        var transEvents     = 'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend '
+        	.replace(/\s/g,eventNs);
+        var animEvents      = 'animationend oAnimationEnd webkitAnimationEnd '
+        	.replace(/\s/g,eventNs);
         
-        setTimeout(function(){
-    		exec();
-    	}, delayMs ? delayMs : 0);
+
+        var bind = function() {
+            var eventsToTrigger = transEvents+' '+animEvents;
+
+            listenTo.off(eventsToTrigger).one(eventsToTrigger, function(){
+                listenTo.off(eventsToTrigger);
+                finishedFunc($elem);
+            });
+
+
+            modifierFunc($elem);
+
+            var animDuration    = parseFloat(listenTo.css('animation-duration'))*1000;
+            var transDuration   = parseFloat(listenTo.css('transition-duration'))*1000;
+            
+            if (isNaN(animDuration)) {
+                animDuration = 0;
+            }
+
+            if (isNaN(transDuration)) {
+                transDuration = 0;
+            }
+
+            if (transDuration==0 && animDuration==0) {
+                listenTo.off(eventsToTrigger);
+                finishedFunc($elem);
+            }
+
+            if (opts.useDuration) {
+                //uses duration instead of ...end-event
+                listenTo.off(eventsToTrigger);
+                var durationMs = Math.max(animDuration,transDuration);
+
+                if (typeof opts.useDuration=='function') {
+                    durationMs = opts.useDuration(durationMs);
+                }
+
+                setTimeout(function(){
+                    finishedFunc($elem);
+                }, durationMs+5);
+            }
+        };
+
+
+        setTimeout(bind, opts.delayMs ? opts.delayMs : 0);        
     },
-    transition: function($elem, addClass, removeClass, finishCallback, delayMs) {
-        this.general($elem, addClass, removeClass, finishCallback, true, delayMs);
-    },
-    animation: function($elem, addClass, removeClass, finishCallback, delayMs) {
-        this.general($elem, addClass, removeClass, finishCallback, false, delayMs);
+    animate: function($elem, modFunc, finFunc, useDuration) {
+        this.process($elem, modFunc, finFunc, {"useDuration": useDuration});
     }
 };
